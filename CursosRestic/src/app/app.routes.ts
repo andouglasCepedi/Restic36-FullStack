13bd208beb8cab1a3d5dbd807da638db9cf5c0b8
andouglasjr <andouglasjr@users.noreply.github.com>
2024-05-17 09:07:34 -0300

    Criação do componente details e criação da rota

--- CursosRestic/src/app/app.routes.ts
+++ CursosRestic/src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { DetailsComponent } from './pages/details/details.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: HomeComponent,
+        title: 'Home Page'
+    },
+    {
+        path: 'details',
+        component: DetailsComponent,
+        title: 'Detalhes do Curso'
+    }
+];
