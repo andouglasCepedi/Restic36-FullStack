629ba0600ec664c8c65f980b8731d0d05d84dd87
andouglasjr <andouglasjr@users.noreply.github.com>
2024-05-17 07:54:57 -0300

    Componentes painel e footer

--- CursosRestic/src/app/app.component.ts
+++ CursosRestic/src/app/app.component.ts
@@ -2,11 +2,13 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { PainelComponent } from './components/painel/painel.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { CursosComponent } from './components/cursos/cursos.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, PainelComponent],
+  imports: [RouterOutlet, HeaderComponent, PainelComponent, FooterComponent, CursosComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ CursosRestic/src/app/components/cursos/cursos.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cursos',
+  standalone: true,
+  imports: [],
+  templateUrl: './cursos.component.html',
+  styleUrl: './cursos.component.css'
+})
+export class CursosComponent {
+
+}
